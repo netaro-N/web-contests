64594b4bc8583d9722d00a4b4953679a4a5aa922
DDRAGON <davide.0829@gmail.com>
2017-08-21 16:09:31 +0900

    テスト

---
+++ move-webcontest2017-summer/光の戦士Mono/js-index.js
@@ -0,0 +1,252 @@
+var storyTextList = new Array(27);
+storyTextList[0] = "クリア！！！";
+storyTextList[1] = "「やあ、また戻ってきたのかい？状況がわかっていない顔だねぇ。覚えていないのかな？」<br>姿が見えないが、声だけははっきりと聞こえる。";
+storyTextList[2] = "「ここは・・・どこ？」<br>見知らぬ場所に混乱し意識しないまま、そう呟いてしまった。見たことのない部屋だ。清潔感のある部屋といえば聞こえはいいが、窓がなく何もない部屋と形容した方が正しい";
+storyTextList[3] = "「そんな冗談のようなこと言うなよー。君と僕の仲じゃないかー？と言っても、ほんとに覚えていないみたいだね」<br>と影は言い放つ。";
+storyTextList[4] = "「状況を説明すると君は前に悔しい思いをして、ここにいるんだよ。選択を迫られているんだ。だから、選んでほしい。」<br>影が言う選択肢とは何か？過去のつらい経験？一体何を言ってるんだ。";
+storyTextList[5] = "「それを信じるも信じないも、君次第だけれど選択肢を選んでくれないかな？」";
+storyTextList[6] = "「ありがとう、では健闘を祈るよ。」<br>影はそういうと、僕の・・・あれ？思い出せない。意識が・・・遠のく・・・・。";
+storyTextList[7] = "・・・・・。<br>・・・・・・・・・・・・。<br>暑い。ここはどこだろうか？いや、記憶が鮮明になりつつある。";
+storyTextList[8] = "ここは野球場の・・・・ベンチ！？さっきの今まで居眠りをしていたのか！？"
+storyTextList[9] = "あぁ、今はっきり思い出した。今まで曖昧だった記憶が嘘のように鮮明になる。";
+storyTextList[10] = "地方の甲子園の決勝戦・・・・・・。<br>そして最後の打席、バッターは自分！<br>この打席で得点を上げなければ目標であった甲子園出場はなくなる！";
+storyTextList[11] = "どうして、あんな夢を見てしまったのか考える余裕はない！<br>折原コーチが頼むぞと言う顔をしてこちらを見ている。";
+storyTextList[12] = "折原コーチは、まるで西洋のグラディエーターの風体をしているが、中身はしっかりとしたコーチである。よくイタリア人と間違えられるらしい。<br>普段以上の気迫でこちらを見つめている。";
+storyTextList[13] = "・・・・バッターボックスに立つ。<br>相手は宿敵ドラゴン高校だ。<br>攻撃守備とも完璧な甲子園出場の常連高校、昨年度は準決勝でドラゴン高校に負けてしまった。";
+storyTextList[14] = "・・・・例え相手が強敵だろうが、今年は違う。去年の敗退の悔しさをバネにみんな今まで努力して来た！";
+storyTextList[15] = "応援歌が響く中、N高マネージャーが応援してくれている。周囲の吹奏楽部の音や、応援団の声にも負けない声だ。<br>彼女もチームの一員、影なる司令塔これまでN高校野球部を支えてくれた。"
+storyTextList[16] = "ここで終わるわけにはいかない！！！！";
+storyTextList[17] = "9回裏２アウト満塁、こちらのチームのピンチでもあり、チャンスでもある。逆転を狙うこともできる。それゆえに相手もその状況は分かっているはずだ。";
+storyTextList[18] = "第１球目・・・。<br>相手の球種を判断し予測する。自分の判断は低めのストライクゾーン・・・・から離れる球。つまり、ボウルのゾーンまだ振りかぶってはいけない。心理的なアドバンテージは相手が有利なはず、ならば最初は様子見で打たなくてもいい球で揺さぶってくる。";
+storyTextList[19] = "審判の合図とともに投手は投げる。<br>違う！直球のストレート、相手は本気で勝負しに来ている！豪速球の球！明らかに自分は・・・振り遅れている。";
+storyTextList[20] = "「ストライク！」審判の声が響く、明らかに自分の判断ミス。相手チームの声援が大きくなる。次は直感で判断するしかない。";
+storyTextList[21] = "第二球目・・・。振りかぶる。<br>「ストライク！」今までの考えが甘かった・・・。９回裏２アウト満塁という状況に、心理的なアドバンテージの優劣なんてない。相手もここで抑えるのに必死だ。中途半端な球種を選択するより、自分の得意かつ自信のあるコースで勝ちに行く判断をしたのだろう。";
+storyTextList[22] = "ここからが対等な勝負！<br>去年の自分とは違う・・・これまで以上に肉体を強化し４番バッターとしてチームに選ばれている。と、自分を鼓舞してみたが不安にかられベンチ側を見る。";
+storyTextList[23] = "・・・・・・。これまでの自分自身を信じるんだ。監督も、マネージャー、チームメイトのみんなの想いがあって今の「僕」がここにいいる。<br>自分自身の決断行動こそが、みんなのためでもあるし、何より後悔しない自分のためだ。なんだか、スッキリした気持ちになる。";//選択肢で変わる文章
+storyTextList[24] = "ドラゴン高校の投手は投球のために振りかぶっている。よく見ると、投手はプレッシャーに我慢しているような顔だった。今まで、どうして気づかなかったのだろうか。";
+storyTextList[25] = "そして、振りかぶった。";
+storyTextList[26] = "試合終了・・・・・・。";
+
+
+var img = new Array();
+img[0] = new Image();
+img[0].src = "img/boku-image.png";
+img[1] = new Image();
+img[1].src = "img/boku-image.png";
+img[2] = new Image();
+img[2].src = "img/boku-image.png";
+img[3] = new Image();
+img[3].src = "img/boku-image.png";
+img[4] = new Image();
+img[4].src = "img/boku-image.png";
+img[5] = new Image();
+img[5].src = "img/boku-image.png";
+img[6] = new Image();
+img[6].src = "img/boku-image.png";
+img[7] = new Image();
+img[7].src = "img/boku-image.png";
+img[8] = new Image();
+img[8].src = "img/boku-image.png";
+img[9] = new Image();
+img[9].src = "img/boku-image.png";
+img[10] = new Image();
+img[10].src = "img/boku-image.png";
+img[11] = new Image();
+img[11].src = "img/kan-image.png";
+img[12] = new Image();
+img[12].src = "img/kan-image.png";
+img[13] = new Image();
+img[13].src = "img/boku-image.png";
+img[14] = new Image();
+img[14].src = "img/boku-image.png";
+img[15] = new Image();
+img[15].src = "img/mana-image.png";
+img[16] = new Image();
+img[16].src = "img/mana-image.png";
+img[17] = new Image();
+img[17].src = "img/boku-image.png";
+img[18] = new Image();
+img[18].src = "img/boku-image.png";
+img[19] = new Image();
+img[19].src = "img/boku-image.png";
+img[20] = new Image();
+img[20].src = "img/boku-image.png";
+img[21] = new Image();
+img[21].src = "img/boku-image.png";
+img[20] = new Image();
+img[20].src = "img/boku-image.png";
+img[21] = new Image();
+img[21].src = "img/boku-image.png";
+img[22] = new Image();
+img[22].src = "img/boku-image.png";
+img[23] = new Image();
+img[23].src = "img/boku-image.png";
+img[24] = new Image();
+img[24].src = "img/boku-image.png";
+img[25] = new Image();
+img[25].src = "img/boku-image.png";
+img[26] = new Image();
+img[26].src = "img/boku-image.png";
+
+
+var imgbg = new Array();
+imgbg[0] = new Image();
+imgbg[0].src = "./img/end.JPEG";
+imgbg[1] = new Image();
+imgbg[1].src = "./img/2-image.JPEG";
+imgbg[2] = new Image();
+imgbg[2].src = "./img/2-image.JPEG";
+imgbg[3] = new Image();
+imgbg[3].src = "./img/2-image.jpeg";
+imgbg[4] = new Image();
+imgbg[4].src = "./img/2-image.jpeg";
+imgbg[5] = new Image();
+imgbg[5].src = "./img/2-image.JPEG";
+imgbg[6] = new Image();
+imgbg[6].src = "./img/3-image.JPEG";
+imgbg[7] = new Image();
+imgbg[7].src = "./img/4-image.JPEG";
+imgbg[8] = new Image();
+imgbg[8].src = "./img/5-image.JPEG";
+imgbg[9] = new Image();
+imgbg[9].src = "./img/6-image.JPEG";
+imgbg[10] = new Image();
+imgbg[10].src = "./img/7-image.JPEG";
+imgbg[11] = new Image();
+imgbg[11].src = "./img/8-image.JPEG";
+imgbg[12] = new Image();
+imgbg[12].src = "./img/8-image.JPEG";
+imgbg[13] = new Image();
+imgbg[13].src = "./img/8-image.JPEG";
+imgbg[14] = new Image();
+imgbg[14].src = "./img/8-image.JPEG";
+imgbg[15] = new Image();
+imgbg[15].src = "./img/8-image.JPEG";
+imgbg[16] = new Image();
+imgbg[16].src = "./img/8-image.JPEG";
+imgbg[17] = new Image();
+imgbg[17].src = "./img/8-image.JPEG";
+imgbg[18] = new Image();
+imgbg[18].src = "./img/8-image.JPEG";
+imgbg[19] = new Image();
+imgbg[19].src = "./img/8-image.JPEG";
+imgbg[20] = new Image();
+imgbg[20].src = "./img/8-image.JPEG";
+imgbg[21] = new Image();
+imgbg[21].src = "./img/8-image.JPEG";
+imgbg[22] = new Image();
+imgbg[22].src = "./img/8-image.JPEG";
+imgbg[23] = new Image();
+imgbg[23].src = "./img/8-image.JPEG";
+imgbg[24] = new Image();
+imgbg[24].src = "./img/8-image.JPEG";
+imgbg[25] = new Image();
+imgbg[25].src = "./img/8-image.JPEG";
+imgbg[26] = new Image();
+imgbg[26].src = "./img/8-image.JPEG";
+
+
+//初期条件
+var i = 0;
+var maxText = storyTextList.length;
+
+//ストーリテキストの行番号初期状態
+var count = 0;
+
+function onRadioButtonChange() {
+	radiobtn1 = document.getElementById("radio01");
+	radiobtn2 = document.getElementById("radio02");
+	target = document.getElementById("output");
+
+	if (radiobtn1.checked == true) {
+		target.innerHTML = "監督が選択されました。<br/>";
+		storyTextList[23] = "監督「自分を信じろっ！」<br>確かにその通りだ。不安であるが自分を信じて打つしかない。";
+		storyTextList[0] = "GAME OVER<br> まだ・・・・終わり・・・・たくない。あの時選択を間違えてしまったのか・・・";
+		img[23] = new Image();
+		img[23].src = "img/kan-image.png";
+		imgbg[0] = new Image();
+		imgbg[0].src = "./img/gameover.JPEG";
+	}
+	else if (radiobtn2.checked == true) {
+		target.innerHTML = "マネージャーが選択されました。<br/>";
+		storyTextList[23] = "マネージャー「ウホウホウーッホ！（ソースコードを見なさい！）」<br>テンションが上がるとゴリラ語になるマネージャの癖が出てしまっている。・・・とにかく打つしかない。";
+		storyTextList[0] = "GAME OVER<br> まだ・・・・終わり・・・・たくない。あの時、選択を間違えてしまったのか・・・";
+		img[23] = new Image();
+		img[23].src = "img/mana-image.png";
+		imgbg[0] = new Image();
+		imgbg[0].src = "./img/gameover.JPEG";
+	} else{
+		storyTextList[0] = "クリア！！！このゲームにお付き合いいただきありがとう！";
+		storyTextList[23] = "・・・・・・。これまでの自分自身を信じるんだ。監督も、マネージャー、チームメイトのみんなの想いがあって今の「僕」がここにいいる。<br>自分自身の決断行動こそが、みんなのためでもあるし、何より後悔しない自分のためだ。なんだか、スッキリした気持ちになる。";//選択肢で変わる文章
+		img[23] = new Image();
+		img[23].src = "img/boku-image.png";
+		imgbg[0] = new Image();
+		imgbg[0].src = "./img/end.JPEG";
+	}
+}
+
+//配列ストーリテキストを1文字ずつ表示
+function pptext(count) {
+	//文章の長さを条件にして、substringメソッドで表示
+	if (storyTextList[count].length >= i) {
+		document.getElementById("viewArea").innerHTML = storyTextList[count].substring(0, i);
+		//表示速度をここで設定
+		timer1 = setTimeout("pptext(count)", 80);
+		i = i + 1;
+	}
+	else {
+		i = 0;
+		clearTimeout(timer1);
+	}
+}
+
+function hyoji(count) {
+	if (count == 5) {
+		document.getElementById("option1").style.display = "block";
+		document.getElementById("gazo").style.display = "none";
+		document.getElementById("gazo-bg").style.display = "none";
+	} else if (count == 1) {
+		document.getElementById("intro").style.display = "none";
+		document.getElementById("gazo").style.display = "none";
+		onRadioButtonChange();
+	} else if (count == 11) {
+		document.getElementById("gazo").style.display = "block";
+		document.getElementById("intro").style.display = "none";
+	} else if (count == 12) {
+		document.getElementById("gazo").style.display = "block";
+		document.getElementById("intro").style.display = "none";
+	} else if (count == 23) {
+		document.getElementById("gazo").style.display = "block";
+		document.getElementById("intro").style.display = "none";
+	} else if (count == 0) {
+		
+		target = document.getElementById("output");
+		target.innerHTML = "自分を信じる<br/>";
+		radiobtn1.checked = false;
+		radiobtn2.checked = false;
+		
+	} else {
+		document.getElementById("option1").style.display = "none";
+		document.getElementById("gazo").style.display = "none";
+		document.getElementById("gazo-bg").style.display = "block";
+		document.getElementById("intro").style.display = "none";
+		document.getElementById("viewArea").style.display = "block";
+	}
+}
+
+function getNumber() {
+	if (count == maxText-1) {//ここの条件は行列の最大値にして文章が最初に戻る
+		count = 0;
+	} else {
+		count++;
+	}
+	//表示させるかどうかの判断
+	hyoji(count);
+	//pptextでストーリー文章の行列を1文字ずつ表示させる
+	pptext(count);
+	//gazoでストーリーの人物画像を表示させる(png)
+	document.getElementById("gazo").src = img[count].src;
+	//gazo-bgでストーリーの背景画像を表示させる(jpeg)
+	document.getElementById("gazo-bg").src = imgbg[count].src;
+}
+
