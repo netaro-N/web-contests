4268dcc08aa5f51bedce58157a911e0883bd4444
DDRAGON <davide.0829@gmail.com>
2018-01-31 22:31:23 +0900

    作品集ページの作成

---
+++ webcontest2017-winter/snow.js
@@ -0,0 +1,7 @@
+var snow = document.createElement("span");
+snow.innerHTML = "●";
+snow.style.position = 'absolute';
+snow.style.color = 'green';
+snow.style.top = '50px';
+snow.style.left = '50px';
+document.getElementById('snow').appendChild(snow);
